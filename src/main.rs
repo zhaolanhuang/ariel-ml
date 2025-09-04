04e6f4d7c61479a530b807f5d57873c759a67c7c
Zhaolan Huang <zhaolan94@gmail.com>
2025-09-04 13:01:28 +0200

    resnet50 poc on native

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,63 @@
 #![no_main]
 #![no_std]
 
-use ariel_os::debug::{exit, log::info, ExitCode};
+use eerie::runtime::{
+    hal::{BufferMapping, BufferView},
+    vm::List,
+};
+extern crate alloc;
+use ariel_os::debug::{exit, log::info, println, ExitCode};
+
+// static MODEL_BYTECODE: &[u8] = include_bytes!("../resnet50.vmfb");
+
+use alloc::vec::Vec;
+fn run(vmfb: &[u8], image_bin: &[f32]) -> Vec<f32> {
+    use eerie::runtime;
+    use eerie::runtime::vm::ToRef;
+
+    let instance = runtime::api::Instance::new(
+        &runtime::api::InstanceOptions::new(&mut runtime::hal::DriverRegistry::new())
+            .use_all_available_drivers(),
+    )
+    .unwrap();
+    let device = instance
+        .try_create_default_device("local-sync")
+        .expect("Failed to create device");
+    let session = runtime::api::Session::create_with_device(
+        &instance,
+        &runtime::api::SessionOptions::default(),
+        &device,
+    )
+    .unwrap();
+    println!("vmfb size: {}", vmfb.len());
+    unsafe { session.append_module_from_memory(vmfb) }.unwrap();
+    let function = session.lookup_function("module.serving_default").unwrap();
+    let input_list =
+        runtime::vm::DynamicList::<runtime::vm::Ref<runtime::hal::BufferView<f32>>>::new(
+            1, &instance,
+        )
+        .unwrap();
+    let input_buffer = runtime::hal::BufferView::<f32>::new(
+        &session,
+        &[1, 224, 224, 3],
+        runtime::hal::EncodingType::DenseRowMajor,
+        image_bin,
+    )
+    .unwrap();
+    let input_buffer_ref = input_buffer.to_ref(&instance).unwrap();
+    input_list.push_ref(&input_buffer_ref).unwrap();
+    let output_list =
+        runtime::vm::DynamicList::<runtime::vm::Ref<runtime::hal::BufferView<f32>>>::new(
+            1, &instance,
+        )
+        .unwrap();
+    function.invoke(&input_list, &output_list).unwrap();
+    let output_buffer_ref = output_list.get_ref(0).unwrap();
+    let output_buffer: BufferView<f32> = output_buffer_ref.to_buffer_view(&session);
+    let output_mapping = BufferMapping::new(output_buffer).unwrap();
+    let out = output_mapping.data().to_vec();
+    out
+}
 
 #[ariel_os::task(autostart)]
 async fn main() {
@@ -9,5 +65,9 @@ async fn main() {
         "Hello from main()! Running on a {} board.",
         ariel_os::buildinfo::BOARD
     );
+    let image_bin: [f32; 224*224*3] = [1.0; 224*224*3];
+    static MODEL_BYTECODE: &[u8] = include_bytes!("../resnet50.vmfb");
+    let output = run(&MODEL_BYTECODE, &image_bin);
+    output.iter().for_each(|x| info!("output:{}", x));
     exit(ExitCode::SUCCESS);
 }
